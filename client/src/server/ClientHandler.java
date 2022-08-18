7d251a9bd8fb1122fe2e3bb61be617866b869e3c
mappuboy <pituvaraliyange@gmail.com>
2022-08-18 18:38:38 +0530

    removing the server module

--- client/src/server/ClientHandler.java
+++ client/src/server/ClientHandler.java
@@ -1,10 +1,7 @@
-
+package server;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
